2d268cbfb6957b95898befc80efd9a62b5b4fe0d
Marina <marinampq7@gmail.com>
2022-10-19 15:51:45 -0300

    user messages in page header logo

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,11 +1,12 @@
 import logoSrc from '../../assets/logo.svg';
+import { messages } from '../../messages';
 
 import styles from './Header.module.css';
 
 export function Header() {
   return (
     <header className={styles.header}>
-      <img className={styles.logo} src={logoSrc} alt="ToDo App Logo" />
+      <img className={styles.logo} src={logoSrc} alt={messages.logo.alt} />
 
       <div className={styles.appName}>
         <span>to</span>
