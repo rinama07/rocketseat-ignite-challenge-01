188b6b6c268a8a081347ec39d2797e9c28d022ad
Marina <marinampq7@gmail.com>
2022-10-19 15:50:37 -0300

    create task interface and some mock

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,18 @@
+import { useEffect, useState } from 'react';
+
 import { Header } from './components/Header/Header';
 import { NewTaskForm } from './components/NewTaskForm/NewTaskForm';
+import { tasks } from './mock/tasks';
+import { Task } from './types/task';
 
 export function App() {
+  const [userTasks, setUserTasks] = useState<Task[]>([]);
+
+  useEffect(() => {
+    setTimeout(() => setUserTasks(tasks), 2000);
+    console.log(userTasks);
+  }, []);
+
   return (
     <>
       <Header />
---
+++ src/mock/tasks.ts
@@ -0,0 +1,18 @@
+import { Task } from '../types/task';
+
+export const tasks: Task[] = [
+  {
+    id: 1,
+    text: 'Integer urna interdum massa libero auctor neque turpis turpis semper. Duis vel sed fames integer.',
+  },
+  {
+    id: 2,
+    text: 'Integer urna interdum massa libero auctor neque turpis turpis semper.',
+    isDone: true,
+  },
+  {
+    id: 3,
+    text: 'Integer urna interdum massa libero auctor.',
+    isDone: false,
+  },
+];
