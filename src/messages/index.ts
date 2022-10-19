f2d9159611cc9cdba2ad2d9de56972e4ca37af31
Marina <marinampq7@gmail.com>
2022-10-19 15:50:59 -0300

    create messages file

---
+++ src/messages/index.ts
@@ -0,0 +1,38 @@
+export const messages = {
+  logo: {
+    alt: 'Logo do ToDo List',
+  },
+  newTask: {
+    input: {
+      placeholder: 'Adicionar uma nova tarefa',
+    },
+    button: {
+      label: 'Criar',
+      title: 'Criar tarefa',
+    },
+  },
+  taskList: {
+    header: {
+      createdLabel: 'Tarefas criadas',
+      doneLabel: 'Concluídas',
+    },
+    content: {
+      empty: {
+        icon: 'Lista de tarefas vazia',
+        text: [
+          'Você ainda não tem tarefas cadastradas',
+          'Crie tarefas e organize seus itens a fazer',
+        ],
+      },
+      list: {
+        status: {
+          done: 'Marcar tarefa como concluída',
+          undone: 'Desmarcar tarefa como concluída',
+        },
+        delete: {
+          icon: 'Remover tarefa',
+        },
+      },
+    },
+  },
+};
